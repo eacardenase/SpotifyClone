5bb5fe6b878ae9dcbeb1a02a0132ef348184e800
Edwin Cardenas <ecardenas@lwolf.com>
2025-08-13 11:59:17 -0500

    Initial Commit

---
+++ SpotifyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Edwin Cardenas on 8/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
